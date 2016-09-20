2d13c5392ade14e99dbd9f12c9585ff98e52f274
Clayton Harlan <claytonharlan3201@gmail.com>
2016-09-20 09:48:54 -0500

    Added Emoji Array to viewController

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBOutlet weak var emojiTable: UITableView!
     
+    var emojiAnimalIcons = ["🐱","🐭","🐻","🐼","🐨","🦁","🐵","🐔","🐰","🐶"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +23,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         emojiTable.dataSource = self
         emojiTable.delegate = self
         
-        
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojiAnimalIcons.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojiAnimalIcons[indexPath.row]
+        return cell
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
